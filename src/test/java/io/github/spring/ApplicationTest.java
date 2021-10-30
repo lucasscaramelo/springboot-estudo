78585c2bf3766c85c95f61702dcecedf6f5f6205
Lucas Scaramelo <lucas.scaramelo@gmail.com>
2021-10-30 00:54:53 -0300

    :white_check_mark: Incluindo teste de contexto

--- src/test/java/io/github/spring/ApplicationTest.java
+++ src/test/java/io/github/spring/ApplicationTest.java
@@ -1,5 +1,13 @@
 package io.github.spring;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class ApplicationTest {
+
+    @Test
+    public void contextLoads(){
+    }
+
 }
